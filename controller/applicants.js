2257f9f10ac6e67b263655b93a0d5060e4091d22
manasvi <manasvi111.kaplay@gmail.com>
2020-07-29 21:59:20 +0530

    29th july 3

--- controller/applicants.js
+++ controller/applicants.js
@@ -13,12 +13,26 @@ router.post('/form',function(req,res){
     req.session.district=district;
     req.session.block = block;
     req.session.panchayat = panchayat;
-    var pagedata={"pagename":"applicants","title":"Application form",state:state,year:year,district:district,block:block,panchayat:panchayat}
+    queries.distinctWhere(state,"Villages",{Panchayat:panchayat},function(err,result){
+        if(err){
+            console.log(error)
+        }
+        if(result){
+            var pagedata={"pagename":"applicants","title":"Application form",state:state,year:year,district:district,block:block,panchayat:panchayat,result:result}
             res.render("layout",pagedata);
+        }
+    })
 })
 router.post('/add',function(req,res){
-    var Applicant_Detail=[]
-    console.log("req.body in applicants...",req.body)
+    var state=req.session.state;
+    var Applicant_Detail=[];
+    var District=req.session.district;
+    var Block=req.session.block;
+    var Panchayat=req.session.panchayat;
+    var Villages=req.body.Villages;
+    var Family_Head=req.body.Family_Head;
+    var Caste=req.body.Caste;
+    var F_H_Name=req.body.F_H_Name;
     var Name=req.body.Name;
     var Gender=req.body.Gender;
     var Age=req.body.Age;
@@ -29,12 +43,32 @@ router.post('/add',function(req,res){
     var Job_Card_Id=req.body.Job_Card_Id;
     var Reason=req.body.Reason;
     var Status=req.body.Status;
+    var Aadhaar=req.body.Aadhaar;
+    if(Name.length==1){
+        Applicant_Detail.push({"Name":Name,"Gender":Gender,"Age":Age,
+        "Request_for_Registration":Request_for_Registration,
+        "Date_of_Job_card_Issue":Date_of_Job_card_Issue,"Disabled":Disabled,
+"Minority":Minority,"Job_Card_Id":Job_Card_Id,"Reason":Reason,"Status":Status,"Aadhaar":Aadhaar})
+    }else{
     for(var i=0;i<Name.length;i+=1){
         Applicant_Detail.push({"Name":Name[i],"Gender":Gender[i],"Age":Age[i],
         "Request_for_Registration":Request_for_Registration[i],
         "Date_of_Job_card_Issue":Date_of_Job_card_Issue[i],"Disabled":Disabled[i],
-"Minority":Minority[i],"Job_Card_Id":Job_Card_Id[i],"Reason":Reason[i],"Status":Status[i]})
+"Minority":Minority[i],"Job_Card_Id":Job_Card_Id[i],"Reason":Reason[i],"Status":Status[i],"Aadhaar":Aadhaar[i]})
     }
-    console.log("Applicant_Detail array......",Applicant_Detail)
+}
+var doc={"District":District,"Block":Block,"Panchayat":Panchayat,"Villages":Villages,"Family_Head":Family_Head,"Caste":Caste,
+"F_H_Name":F_H_Name,"Applicant_Detail":Applicant_Detail}
+console.log("Final doc to be inserted.....",doc)
+    queries.insert(state,doc,function(err,result){
+        if(err){
+            console.log(err)
+        }
+        if(result){
+            //req.flash('info','Applicant added')
+             //res.redirect('back')
+             res.send("Applicant added")
+        }
+    })
 })
 module.exports=router;
